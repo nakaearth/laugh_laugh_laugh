ea9defbbadb54595de2ffb55cbfd38b62040b409
Nakamura shinichirou <naka5313@gmail.com>
2012-04-08 00:55:33 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaughLaugh::Application.config.secret_token = 'd4ae3d4724593bec74909780cf7e75323456d2e607812388204ab0ef1c9a6aa8a6b35fe325ee87af42f300e85689edb6afff387d256e5abcbd116c20b063273d'
