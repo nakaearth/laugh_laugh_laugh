763c5926feccd8ec40f61ffce84cdf405d18f300
Nakamura shinichirou <naka5313@gmail.com>
2012-04-24 01:31:47 +0900

    facebook ログインの導線を追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :twitter, 'Jn1parDOEgcmc3KrYt3zA','4FzkDgOz18DvjYSD9SH7MygCHQSUsnFTbJ1lH8NYj0'
+  provider :facebook, 'face_book_key','face_book_keys'
 end
