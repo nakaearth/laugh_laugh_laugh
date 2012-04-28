07f86dcc9ff22476eac66a0af7025965bd6eb939
Nakamura shinichirou <naka5313@gmail.com>
2012-04-28 15:01:00 +0900

    update

--- app/controllers/social_controller.rb
+++ app/controllers/social_controller.rb
@@ -4,7 +4,9 @@ class SocialController < ApplicationController
   include RestGraph::RailsUtil
   def index
     access_token=current_user.token
+    p access_token
     rg = RestGraph.new(:access_token=>access_token)
+    p rg
     @mes=rg.get('me')
     rg.get('me/likes')
   end
