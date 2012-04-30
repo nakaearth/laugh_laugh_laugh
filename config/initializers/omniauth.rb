bb6ff6c791e73a7c41b73ac95271c60234fe1990
Nakamura shinichirou <naka5313@gmail.com>
2012-05-01 06:02:02 +0900

    omniauth config update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :twitter, 'Jn1parDOEgcmc3KrYt3zA','4FzkDgOz18DvjYSD9SH7MygCHQSUsnFTbJ1lH8NYj0'
-  #provider :facebook, ENV['FACEBOOK_APP_ID'],ENV['FACEBOOK_SECRET_KEY'],{:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  provider :facebook, ENV['FACEBOOK_APP_ID'],ENV['FACEBOOK_SECRET_KEY'],{:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
