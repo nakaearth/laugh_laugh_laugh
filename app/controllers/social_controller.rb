b436a8b9620971140fa8eef516346283ae7aa6ea
Nakamura shinichirou <naka5313@gmail.com>
2012-05-04 10:31:32 +0900

    update

--- app/controllers/social_controller.rb
+++ app/controllers/social_controller.rb
@@ -2,7 +2,7 @@ class SocialController < ApplicationController
 
   def index
     @user = User.find(session[:user_id])
-    @facebook_user=FbGraph::User.me(@user.name,@user.token)
+    @facebook_user=FbGraph::User.me(@user.name,:access_token=>@user.token)
     p @user.name
     p @facebook_user.name
   end
