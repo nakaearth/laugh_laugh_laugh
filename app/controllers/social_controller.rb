375adefeb9f52d9932f28c0c6fbe10c2ecd56a10
Nakamura shinichirou <naka5313@gmail.com>
2012-05-04 10:58:40 +0900

    update

--- app/controllers/social_controller.rb
+++ app/controllers/social_controller.rb
@@ -5,5 +5,9 @@ class SocialController < ApplicationController
     @facebook_user=FbGraph::User.new(@user.name,:access_token=>@user.token)
     p @user.name
     p @facebook_user.identifier
+    @facebook_user.fetch
+    @albums=@facebook_user.albums
+    p @albums
+    p @facebook_user.friends
   end
 end
